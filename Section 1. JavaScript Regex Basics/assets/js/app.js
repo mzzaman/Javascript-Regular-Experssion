aa8b3773193144e7e9a4deef068a280ad1c42f5f
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-08 22:35:16 +0600

    Javascript Regular Expression

--- Section 1. JavaScript Regex Basics/assets/js/app.js
+++ Section 1. JavaScript Regex Basics/assets/js/app.js
@@ -7,3 +7,35 @@ let re2 = /hi/;
 let result = re2.test("hi John");
 
 console.log(result);
+
+// 1) The ignore flag (i)
+const ignoreFlag = /hi/i;
+const show = ignoreFlag.test("Hi John");
+console.log(show); // true;
+
+let reIgnore = new RegExp("hi", "i");
+let ignoreShow = reIgnore.test("Hi John");
+console.log(ignoreShow); // true;
+
+// 2) The global flag (g)
+let message = "Hi, are you there? hi, HI...";
+let element = /hi/gi;
+let matches = [];
+let match;
+do {
+  match = element.exec(message);
+  if (match) {
+    matches.push(match);
+  }
+} while (match != null);
+console.dir(matches);
+
+// Searching strings
+
+let str = "Are you Ok? Yes, I'm Ok";
+let resultOk = str.match(/Ok/gi);
+console.log(resultOk);
+
+// Replacing strings;
+let replaceResult = str.replace(/Ok/gi, "Fine");
+console.log(replaceResult);
