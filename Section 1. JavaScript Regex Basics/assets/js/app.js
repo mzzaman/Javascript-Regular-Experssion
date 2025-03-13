f5a45480194ab81aa6627d600b854265b7f64e4c
moniruzzaman <moniruzzamanm734@gmail.com>
2025-03-13 11:09:34 +0600

    Javascript Regular Expression Basic

--- Section 1. JavaScript Regex Basics/assets/js/app.js
+++ Section 1. JavaScript Regex Basics/assets/js/app.js
@@ -39,3 +39,38 @@ console.log(resultOk);
 // Replacing strings;
 let replaceResult = str.replace(/Ok/gi, "Fine");
 console.log(replaceResult);
+
+// Regular Expression: Character Classes
+
+let phone = "+1-(408)-555-0105";
+let res = /\d/;
+
+console.log(phone.match(res));
+
+let res2 = /\d/g;
+let number = phone.match(res2);
+let phoneNumber = number.join("");
+console.log(phoneNumber);
+
+let strTwo = "02 is oxygen";
+let reTwo = /\w\d/g;
+console.log(strTwo.match(reTwo));
+
+let strThree = "ES6 Tutorial";
+let reThree = /ES\d/g;
+console.log(strThree.match(reThree));
+
+// Regular Expression: Anchors;
+let language = "Javascript";
+console.log(/^J/.test(language)); // matches the first character;
+console.log(/t$/.test(language)); // matches the last character;
+
+// Regular Expression: Word Boundaries
+
+console.log("Hello, JS!".match(/\bJS\b/));
+console.log("Hello, JSscript!".match(/JS/));
+
+console.log("ES 2015".match(/\b\d\d\d\d\b/));
+
+let strFour = "I start coding JS at 05:30 AM";
+console.log(strFour.match(/\b\d\d:\d\d\b/));
